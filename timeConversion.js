c7e1688fb3d4f52f3d4e66bca90995ed0c4c51f0
Arturo <artur18_02@hotmail.com>
2023-03-09 13:43:15 -0600

    done

---
+++ timeConversion.js
@@ -0,0 +1,15 @@
+function timeConversion(s) {
+    let hours = parseInt(s.slice(0, 2));
+    let minutes = parseInt(s.slice(3, 5));
+    let seconds = parseInt(s.slice(6, 8));
+    let am_pm = s.slice(-2).toUpperCase();
+  
+    if (am_pm === "AM") {
+      if (hours === 12) {
+        hours = 0;
+      }
+    } else {
+      if (hours !== 12) {
+        hours += 12;
+      }
+    }
