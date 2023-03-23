eaa82707b7de95306d92f192a035df4a5ad653ef
Arturo <artur18_02@hotmail.com>
2023-03-23 16:06:06 -0500

    done

--- isPalindrome.js
+++ isPalindrome.js
@@ -2,6 +2,6 @@ var isPalindrome = function(s) {
     let str = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
 
     let reverseStr = str.split('').reverse().join('');
-//return..
+//return.
 return str==reverseStr;
 };
