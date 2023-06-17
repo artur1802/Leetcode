c4116367439c29fd1b8fb8e5e92112096a6d329a
Arturo <artur18_02@hotmail.com>
2023-06-16 19:46:53 -0500

    --date=08-08-2022

--- 02Anagram.js
+++ 02Anagram.js
@@ -1,6 +1,6 @@
 var isAnagram = function(s, t) {
     const sortedS = s.toLowerCase().split('').sort().join('');
   const sortedT = t.toLowerCase().split('').sort().join('');
-
+////
   return sortedS === sortedT;
 };
