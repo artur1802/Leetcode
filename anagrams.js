7b8ce415f8ec8abbb121cf833d80274dd034fd32
Arturo <artur18_02@hotmail.com>
2023-06-27 22:09:25 -0500

    done

---
+++ anagrams.js
@@ -0,0 +1,17 @@
+var groupAnagrams = function(strs) {
+    
+    const map={}
+        for(const s of strs){
+    
+    const key= s.split('').sort().join('');
+    if(map[key]){
+    
+        map[key].push(s);
+    }
+    else{
+    
+        map[key]=[s];
+    }
+        }
+        return Object.values(map);
+    };
