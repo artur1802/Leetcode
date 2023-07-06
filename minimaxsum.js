be5e065abd554256f3de588e88a12178241a7328
Arturo <artur18_02@hotmail.com>
2023-07-06 11:10:28 -0500

    done

---
+++ minimaxsum.js
@@ -0,0 +1,17 @@
+function miniMaxSum(arr) {
+    // Write your code here
+    const newarr= arr.sort((a,b)=>a-b);
+    let minsum=0;
+    let maxsum=0;
+    for(let i=0; i<newarr.length-1;i++){
+    minsum+=newarr[i];
+        
+    }
+    
+ for(let i=1; i<newarr.length;i++){
+    maxsum+=newarr[i];
+        
+    }
+    
+    console.log(minsum+" "+maxsum)
+}
